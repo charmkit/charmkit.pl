0413eb9fa8b18fdb98bc30d7c2c6b7190003fe2c
Adam Stokes <battlemidget@users.noreply.github.com>
2016-11-21 12:38:45 -0500

    update charmkit DL version
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- src/setup.sh
+++ src/setup.sh
@@ -2,4 +2,4 @@
 
 apt-get update
 apt-get install -qyf cpanminus build-essential libssl-dev libxml2-dev libexpat1-dev
-cpanm -n http://cpan.metacpan.org/authors/id/A/AD/ADAMJS/App-CharmKit-2.03.tar.gz
+cpanm -n http://cpan.metacpan.org/authors/id/A/AD/ADAMJS/App-CharmKit-2.07.tar.gz
